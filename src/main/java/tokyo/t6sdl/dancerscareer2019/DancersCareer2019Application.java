a1004a97d363f34989cd163e019ab1964cd78f3a
藤田晃弘 <Akihiro@fuji-mbp.local>
2018-10-31 16:45:18 +0900

    for product

--- src/main/java/tokyo/t6sdl/dancerscareer2019/DancersCareer2019Application.java
+++ src/main/java/tokyo/t6sdl/dancerscareer2019/DancersCareer2019Application.java
@@ -1,6 +1,6 @@
 package tokyo.t6sdl.dancerscareer2019;
 
-import org.flywaydb.core.Flyway;
+//import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
@@ -16,10 +16,11 @@ public class DancersCareer2019Application {
 //		flyway.setDataSource("jdbc:mysql://us-cdbr-iron-east-01.cleardb.net/heroku_6ed62eaddfd562d", "b23da6ad7dabc5", "f5f14f28");
 //		flyway.repair();
 //		flyway.clean();
-		Flyway flyway = new Flyway();
-		flyway.setDataSource("jdbc:mysql://us-cdbr-iron-east-01.cleardb.net/heroku_355da0dbefbdd1d", "ba75598b86a005", "30b5c174");
-		flyway.repair();
-		flyway.clean();
+//		--product--
+//		Flyway flyway = new Flyway();
+//		flyway.setDataSource("jdbc:mysql://us-cdbr-iron-east-01.cleardb.net/heroku_355da0dbefbdd1d", "ba75598b86a005", "30b5c174");
+//		flyway.repair();
+//		flyway.clean();
 		SpringApplication.run(DancersCareer2019Application.class, args);
 	}
 	
