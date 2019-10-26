c94f860a370584777697e341b81a946af897e481
ut-aki <heihiga@gmail.com>
2019-10-26 20:04:00 +0900

    [modify] local開発環境整備

--- src/main/java/tokyo/t6sdl/dancerscareer2019/DancersCareer2019Application.java
+++ src/main/java/tokyo/t6sdl/dancerscareer2019/DancersCareer2019Application.java
@@ -1,5 +1,7 @@
 package tokyo.t6sdl.dancerscareer2019;
 
+import java.util.ResourceBundle;
+
 //import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,12 +15,18 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class DancersCareer2019Application {
 
 	public static void main(String[] args) {
-//		if (System.getenv("SPRING_PROFILES_ACTIVE").equals("staging")) {
+//		if (System.getProperty("SPRING_PROFILES_ACTIVE", "staging").equals("staging")) {
 //			Flyway flyway = new Flyway();
 //			flyway.setDataSource(System.getenv("DB_URL_JDBC"), System.getenv("DB_USERNAME"), System.getenv("DB_PASSWORD"));
 //			flyway.repair();
 //			flyway.clean();			
 //		}
+		if (System.getProperty("SPRING_PROFILES_ACTIVE", "development").equals("development")) {
+			ResourceBundle rb = ResourceBundle.getBundle("application-development");
+			System.setProperty("DB_URL_JDBC", rb.getString("db.url.jdbc"));
+			System.setProperty("DB_USERNAME", rb.getString("db.username"));
+			System.setProperty("DB_PASSWORD", rb.getString("db.password"));
+		}
 		SpringApplication.run(DancersCareer2019Application.class, args);
 	}
 	
