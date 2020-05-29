4cc4d74f2275fca0175b8b8ba8a226327d8217ae
ut-aki <heihiga@gmail.com>
2020-05-30 00:06:52 +0900

    [update] その他諸々のデザイン修正

--- src/main/java/tokyo/t6sdl/dancerscareer2019/model/form/NewPasswordForm.java
+++ src/main/java/tokyo/t6sdl/dancerscareer2019/model/form/NewPasswordForm.java
@@ -1,17 +1,28 @@
 package tokyo.t6sdl.dancerscareer2019.model.form;
 
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.Data;
-import tokyo.t6sdl.dancerscareer2019.validation.SamePassword;
 
 @Data
-@SamePassword
 public class NewPasswordForm {
 	private String currentPassword;
+	@NotBlank
 	@Size(min=8)
-	@Pattern(message="半角英数字、記号（!.?+$%#&*=@）をそれぞれ最低1文字ずつ含む必要があります", regexp="^(?=[a-zA-Z0-9]*(\\.|!|\\?|\\+|\\$|%|#|&|\\*|=|@))(?=([a-zA-Z]|\\.|!|\\?|\\+|\\$|%|#|&|\\*|=|@)*[0-9])(?=([0-9]|\\.|!|\\?|\\+|\\$|%|#|&|\\*|=|@)*[a-zA-Z])(\\w|\\.|!|\\?|\\+|\\$|%|#|&|\\*|=|@)*$")
-	private String newPassword;
-	private String confirm;
+	@Pattern(message="半角英数字、記号（!?.+=*$%#&@）をそれぞれ最低1文字ずつ含む必要があります", regexp="^(?=[a-zA-Z0-9]*(\\.|!|\\?|\\+|\\$|%|#|&|\\*|=|@))(?=([a-zA-Z]|\\.|!|\\?|\\+|\\$|%|#|&|\\*|=|@)*[0-9])(?=([0-9]|\\.|!|\\?|\\+|\\$|%|#|&|\\*|=|@)*[a-zA-Z])(\\w|\\.|!|\\?|\\+|\\$|%|#|&|\\*|=|@)*$")
+	private String newPassword = "";
+	@NotBlank
+	private String confirmPassword = "";
+	
+	@AssertTrue(message="パスワードが異なります")
+	public boolean isSamePassword() {
+		if(newPassword.equals(confirmPassword)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
