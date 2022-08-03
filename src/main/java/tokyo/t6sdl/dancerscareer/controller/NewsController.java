ef3503b9315f42f2a7eba045e264fc918afa7cdc
Akihiro Fujita <heihiga@gmail.com>
2022-08-03 20:54:21 +0900

    Fix conflicts

--- src/main/java/tokyo/t6sdl/dancerscareer/controller/NewsController.java
+++ src/main/java/tokyo/t6sdl/dancerscareer/controller/NewsController.java
@@ -43,13 +43,9 @@ import tokyo.t6sdl.dancerscareer.service.SecurityService;
 public class NewsController {
 	private final SecurityService securityService;
 	private final AccountService accountService;
-<<<<<<< HEAD
-	private final List<Integer> newsIds = Arrays.asList(1, 21, 31, 41, 51, 61, 71, 81, 91, 101, 121); // NOTE: 10刻みの数値
-=======
 	// NOTE: 10刻みの数値
 	private final List<Integer> newsIds = Arrays.asList(1, 21, 31, 41, 51, 61, 71, 81, 91, 101, 111, 121);
 	// NOTE: 記事一覧ページで表示する短めの記事タイトル。65文字まで。
->>>>>>> 13fa33d1e594386f5ba0df770ccc5655473ca60b
 	private final List<String> newsShortTitles = Arrays.asList(
 			"ダンサー向け就活セミナーが開催されます！",
 			"【19卒ダンキャリ利用者インタビュー第1弾】〜就活ダルいと言っていた僕が今、仕事を楽しんでいる理由〜",
@@ -61,15 +57,10 @@ public class NewsController {
 			"【21卒ダンキャリ利用者インタビュー第1弾】〜キャリア面談を通して身につけた自分の見つめ方、イメージと現実の違いとは〜",
 			"【21卒ダンキャリ利用者インタビュー第2弾】”代表”だけでは通過しない！人事がダンスの話で注目するポイントとは！？",
 			"これで就活は怖くない！〜ダンキャリ活用アドバイス〜",
-<<<<<<< HEAD
-			"【24卒向けセミナー vol.1】 ダンスサークル出身社会人から学ぶ納得内定への最短ルート"
-		); // NOTE: 記事一覧ページで表示する短めの記事タイトル。65文字まで。
-=======
 			"23卒 就活無双×就活苦労 〜同じサークル、同じ役職、同じ志望業界 この二人の間あった”圧倒的差”から見出す”就活の近道”〜",
 			"【24卒向けセミナー vol.1】 ダンスサークル出身社会人から学ぶ納得内定への最短ルート"
 		);
 	// NOTE: 記事最上部に表示する正式な記事タイトル。文字数制限なし。
->>>>>>> 13fa33d1e594386f5ba0df770ccc5655473ca60b
 	private final List<String> newsTitles = Arrays.asList(
 			"ダンサー向け就活セミナーが開催されます！",
 			"【19卒ダンキャリ利用者インタビュー第1弾】〜就活ダルいと言っていた僕が今、仕事を楽しんでいる理由〜",
@@ -81,15 +72,10 @@ public class NewsController {
 			"【21卒ダンキャリ利用者インタビュー第1弾】〜キャリア面談を通して身につけた自分の見つめ方、イメージと現実の違いとは〜",
 			"【21卒ダンキャリ利用者インタビュー第2弾】”代表”だけでは通過しない！人事がダンスの話で注目するポイントとは！？",
 			"これで就活は怖くない！〜ダンキャリ活用アドバイス〜",
-<<<<<<< HEAD
-			"【24卒向けセミナー vol.1】 ダンスサークル出身社会人から学ぶ納得内定への最短ルート"
-		); // NOTE: 記事最上部に表示する正式な記事タイトル。文字数制限なし。
-=======
 			"23卒 就活無双×就活苦労 〜同じサークル、同じ役職、同じ志望業界 この二人の間あった”圧倒的差”から見出す”就活の近道”〜",
 			"【24卒向けセミナー vol.1】 ダンスサークル出身社会人から学ぶ納得内定への最短ルート"
 		);
 	// NOTE: 記事アップロード日。
->>>>>>> 13fa33d1e594386f5ba0df770ccc5655473ca60b
 	private final List<String> newsUpdatedDates = Arrays.asList(
 			"2018-12-06",
 			"2019-12-21",
@@ -101,14 +87,9 @@ public class NewsController {
 			"2021-05-25",
 			"2021-09-20",
 			"2022-06-01",
-<<<<<<< HEAD
-			"2022-08-01"
-		); // NOTE: 記事アップロード日。
-=======
 			"2022-07-15",
 			"2022-08-01"
 		);
->>>>>>> 13fa33d1e594386f5ba0df770ccc5655473ca60b
 	private final Map<Integer, Map<String, Object>> newsMap = generateNewsMap();
 
 	@RequestMapping()
